04b3adf17ac668c1f2171590d7732c59f06d05ba
johnpilbeam <jpilbeam@gmail.com>
2018-05-28 13:38:47 +0100

    Finished Chapter 7

--- collection/views.py
+++ collection/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request): 
-    # this is your new view
-    # return render(request, 'index.html')
-    # defining the variable 
-    number = 6 
-    thing = "Thing name"
-    # passing the variable to the view 
-    return render(request, 'index.html', { 
-        'number': number, 
-        'thing': thing,
+    things = Thing.objects.all()
+    return render( request, 'index.html', {
+    'things': things,
     })
