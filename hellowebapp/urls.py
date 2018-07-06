2098c87ce4cd3fba31ef9dd1938c2e9f761d6f91
johnpilbeam <jpilbeam@gmail.com>
2018-07-06 16:49:07 +0100

    Almost completed chapter 10

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 from collection import views
 
-
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/',
-        TemplateView.as_view(template_name='about.html'), name='about'),
+        TemplateView.as_view(template_name='about.html'), 
+        name='about'),
     path('contact/',
-        TemplateView.as_view(template_name='contact.html'), name='contact'),
-    path('things/(<slug>/', 
-        views.thing_detail, name='thing_detail'),
-    path('things/<slug>/edit/', 
-        views.edit_thing, name='edit_thing'),
+        TemplateView.as_view(template_name='contact.html'), 
+        name='contact'),
+    path('things/<slug>/', views.thing_detail, name='thing_detail'),
+    path('things/<slug>/edit/', views.edit_thing, name='edit_thing'),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
-#    path('accounts/', registration.backends.simple.paths),
 ]
+  
