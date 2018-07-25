56634562006d3d4e00fbafb439076fa0e48a3788
johnpilbeam <jpilbeam@gmail.com>
2018-07-25 16:59:55 +0100

    Part way through Intermediate Chapter 2

--- collection/models.py
+++ collection/models.py
@@ -8,3 +8,15 @@ class Thing(models.Model):
 	slug = models.SlugField(unique = True)
 	user = models.OneToOneField(User, on_delete=models.CASCADE,
 	    blank=True, null = True)
+
+	    
+class Social(models.Model):
+    SOCIAL_TYPES = (
+        ('twitter', 'Twitter'),
+        ('facebook', 'Facebook'),
+        ('pinterest', 'Pinterest'),
+        ('instagram', 'Instagram'),
+    )
+    network = models.CharField(max_length=255, choices=SOCIAL_TYPES)
+    username = models.CharField(max_length=255)
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name="social_accounts")
